6110399c22c3422026b8497ea3640fa177a6c439
Alejandro Ramírez Ramos <ramirez.raale20@aulas.triana>
2020-10-19 13:20:10 +0200

    añadido proyecto

---
+++ BIODAM/src/main/java/com/example/BIODAM/ProyectoBiodamApplication.java
@@ -0,0 +1,13 @@
+package com.example.BIODAM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBiodamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBiodamApplication.class, args);
+	}
+
+}
---
+++ BIODAM/src/test/java/com/example/BIODAM/ProyectoBiodamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BIODAM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBiodamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
